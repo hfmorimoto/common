9f9ac652e3b56e9aea4f26052eb0a9ade578d460
taro_morimoto <taro_morimoto@hot-factory.jp>
2014-01-13 16:35:43 +0900

    common.js
    新規作成

---
+++ common.js
@@ -0,0 +1,11 @@
+﻿$(function(){
+     $("a.scroll").click(function(){
+     $('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow','swing');
+     return false;
+     })
+ $("a.roll img").hover(function(){
+       $(this).fadeTo("fast", 0.6); 
+    },function(){
+       $(this).fadeTo("fast", 1.0); 
+    });
+});

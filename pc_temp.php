f20dc0e49bf4069887b2214c71db51f1d3e1507c
taro_morimoto <taro_morimoto@hot-factory.jp>
2014-01-23 18:58:59 +0900

    pc_temp created

---
+++ pc_temp.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+<meta charset="utf-8">
+
+<title></title>
+<link href="/common/css/reset.css" rel="stylesheet" type="text/css" />
+<link href="/common/css/layout.css" rel="stylesheet" type="text/css" />
+<link href="/css/layout.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body id="top">
+<header>
+  <div class="headerWrap">
+  </div>
+</header>
+<div class="wrap">
+
+  <div id="contents">
+    <article class="contentsInner">
+      <section id="main">
+      
+        
+        
+       
+       
+        
+      </section>
+      <section id="event">
+
+
+      </section>
+
+    </article>
+  </div>
+  <!--contentns--> 
+  
+</div>
+<!--wrap-->
+<footer></footer>
+</body>
+</html>
